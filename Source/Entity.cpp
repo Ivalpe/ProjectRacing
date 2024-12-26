a12552c0058f49354e761f2928acfdbb4748d990
LaraGuevara <159762558+LaraGuevara@users.noreply.github.com>
2024-12-26 18:20:19 +0100

    Commented other TurnBody function
    float returning TurnBody function kept commented in case it is needed

--- Source/Entity.cpp
+++ Source/Entity.cpp
@@ -15,3 +15,16 @@ update_status Entity::Update() {
 
 }
 
+//float Entity::TurnBody(bool isGoingForward, bool isGoingRight, float torque, float speed) const {
+//	float FinalTorque = 0.f;
+//	if (isGoingRight) {
+//		if (isGoingForward) FinalTorque = torque / abs(speed);
+//		else FinalTorque = -torque / abs(speed);
+//	}
+//	else {
+//		if (isGoingForward) FinalTorque = -torque / abs(speed);
+//		else FinalTorque = torque / abs(speed);
+//	}
+//
+//	return FinalTorque;
+//}
