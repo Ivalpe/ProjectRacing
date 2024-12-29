5974b06b0c9f7320369474ae8efaf081d7781dd2
KaiCaire <125651900+KaiCaire@users.noreply.github.com>
2024-12-29 16:29:25 +0100

    added result musics

--- Source/ModuleGame.cpp
+++ Source/ModuleGame.cpp
@@ -45,7 +45,7 @@ bool ModuleGame::Start()
 
 	selectBG = LoadTexture("Assets/Main Menu/selection screen.png");
 	nextBtTex = LoadTexture("Assets/Main Menu/siguiente.png");
-	backBtTex = LoadTexture("Assets/Main Menu/atr�s.png");
+	backBtTex = LoadTexture("Assets/Main Menu/atras.png");
 
 	bluePressX = LoadTexture("Assets/Main Menu/blue Press x to go back.png");
 	bluePressZ = LoadTexture("Assets/Main Menu/blue Press z to confirm.png");
@@ -64,12 +64,16 @@ bool ModuleGame::Start()
 
 	selectedVehicle = LoadTexture("Assets/selectVehicle.png");
 	selectedVehicle2 = LoadTexture("Assets/selectVehicle2.png");
-
+	
 	musicGame.push_back(LoadMusicStream("Assets/Audio/Music/In-game.ogg"));
 	musicGame.push_back(LoadMusicStream("Assets/Audio/Music/Mario-Kart-DS-Waluigi-Pinball.ogg"));
 	musicGame.push_back(LoadMusicStream("Assets/Audio/Music/Rascal Flatts - Life Is a Highway.ogg"));
-	charSelectMusic = LoadMusicStream("Assets/Audio/Music/Character-select.ogg");
+	musicGame.push_back(LoadMusicStream("Assets/Audio/Music/Coconut Mall.ogg"));
+	charSelectMusic = LoadMusicStream("Assets/Audio/Music/Choose Your Character.ogg");
 	mainMenuMusic = LoadMusicStream("Assets/Audio/Music/Main-Menu.ogg");
+	firstPlaceMusic = LoadMusicStream("Assets/Audio/Music/Race Results_1st Place.ogg");
+	secondPlaceMusic = LoadMusicStream("Assets/Audio/Music/Race Results_2nd-4th Place.ogg");
+	fifthPlaceMusic = LoadMusicStream("Assets/Audio/Music/Race Results_5th-8th Place.ogg");
 
 
 	bepSFX = App->audio->LoadFx("Assets/Audio/SFX/bep.wav");
@@ -89,13 +93,6 @@ bool ModuleGame::Start()
 
 	//Random Map
 	std::uniform_int_distribution<std::mt19937::result_type> randMap(0, 3);
-
-
-
-
-
-
-
 
 
 
@@ -412,7 +409,7 @@ void ModuleGame::SelectCharacter() {
 	App->renderer->Draw(selectedVehicle, posVehicles[selectedPos].x, posVehicles[selectedPos].y, &rect);
 	if (TwoPlayerMode)	App->renderer->Draw(selectedVehicle2, posVehicles[selectedPosPlayer2].x, posVehicles[selectedPosPlayer2].y, &rect);
 
-
+	backButton->Update();
 	OnGuiMouseClickEvent(backButton);
 
 	if (TwoPlayerMode) {
@@ -617,8 +614,12 @@ void ModuleGame::Game() {
 		++FinalRankingTracker;
 		PlayerOneFinalPos = FinalRankingTracker;
 
-		if (!TwoPlayerMode) FinishRace = true;
-		else if (PlayerTwoDone) FinishRace = true;
+		if (!TwoPlayerMode) {
+			FinishRace = true;
+		}
+		else if (PlayerTwoDone) {
+			FinishRace = true;
+		}
 	}
 
 	if (TwoPlayerMode) {
@@ -640,16 +641,16 @@ void ModuleGame::Game() {
 
 	if (!PlayerOneDone) car->Render();
 	if (TwoPlayerMode && !PlayerTwoDone) car2->Render();
-	for (auto car : enemyCars) {
-		if (!car->EndedRace) car->Render();
+	for (auto eCar : enemyCars) {
+		if (!eCar->EndedRace) eCar->Render();
 	}
 
 
 	if (timer <= 0) {
 		if (!PlayerOneDone) car->Update();
 		if (TwoPlayerMode && !PlayerTwoDone) car2->Update();
-		for (auto car : enemyCars) {
-			if (!car->EndedRace) car->Update();
+		for (auto eCar : enemyCars) {
+			if (!eCar->EndedRace) eCar->Update();
 		}
 		DrawUI();
 	}
@@ -668,12 +669,19 @@ void ModuleGame::Game() {
 
 	DrawRectangleLines(App->renderer->camera.x, App->renderer->camera.y, SCREEN_WIDTH, SCREEN_HEIGHT, Color({ 0,0,255,255 }));
 
-	if (FinishRace || IsKeyPressed(KEY_F4)) stateGame = RACE_END;
+	if (FinishRace || IsKeyPressed(KEY_F4)) {
+		if (PlayerOneFinalPos == 1) resultsMusic = firstPlaceMusic;
+		else if (PlayerOneFinalPos >= 2 || PlayerOneFinalPos <= 4) resultsMusic = secondPlaceMusic;
+		else if (PlayerOneFinalPos >= 5) resultsMusic = fifthPlaceMusic;
+
+		App->audio->PlayMusic(resultsMusic, 0.5f);
+		stateGame = RACE_END;
+	}
 
 }
 
 void ModuleGame::RaceEnd() {
-	endFontSize = 40.0f;
+	endFontSize = 45.0f;
 	float spacing = 1.0f;
 	Color color = BLACK;
 	double bestTime = xmlFile.child("best").attribute("time").as_double();
@@ -681,9 +689,8 @@ void ModuleGame::RaceEnd() {
 	if (TwoPlayerMode) {
 		DrawTexture(endRaceTwoPlayers, 0, 0, WHITE);
 
-
-		DrawTextEx(gamTex, TextFormat("%.2f", PlayerOneFinalTime), { SCREEN_WIDTH / 2 - 20 , 250 }, endFontSize, spacing, color);
-		DrawTextEx(gamTex, TextFormat("%.2f", PlayerTwoFinalTime), { SCREEN_WIDTH / 2 - 50, 320 }, endFontSize, spacing, color);
+		DrawTextEx(gamTex, TextFormat("%.2f", PlayerOneFinalTime), { SCREEN_WIDTH / 2 - 20 , 240 }, endFontSize, spacing, color);
+		DrawTextEx(gamTex, TextFormat("%.2f", PlayerTwoFinalTime), { SCREEN_WIDTH / 2 - 50, 300 }, endFontSize, spacing, color);
 
 		DrawTextEx(gamTex, TextFormat("%d", PlayerOneFinalPos), { SCREEN_WIDTH / 2 + 120, 430 }, endFontSize, spacing, color);
 		DrawTextEx(gamTex, TextFormat("%d", PlayerTwoFinalPos), { SCREEN_WIDTH / 2 + 120, 500 }, endFontSize, spacing, color);
--- Source/ModulePhysics.cpp
+++ Source/ModulePhysics.cpp
@@ -365,85 +365,89 @@ update_status ModulePhysics::PostUpdate()
 
 	// Bonus code: this will iterate all objects in the world and draw the circles
 	// You need to provide your own macro to translate meters to pixels
+	
 	for (b2Body* b = world->GetBodyList(); b; b = b->GetNext())
 	{
-		for (b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext())
-		{
-			switch (f->GetType())
+		if (b->IsEnabled()) {
+			for (b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext())
 			{
-				// Draw circles ------------------------------------------------
-			case b2Shape::e_circle:
-			{
-				b2CircleShape* shape = (b2CircleShape*)f->GetShape();
-				b2Vec2 pos = f->GetBody()->GetPosition();
-
-				DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), (float)METERS_TO_PIXELS(shape->m_radius), Color{ 0, 0, 0, 128 });
-			}
-			break;
-
-			// Draw polygons ------------------------------------------------
-			case b2Shape::e_polygon:
-			{
-				b2PolygonShape* polygonShape = (b2PolygonShape*)f->GetShape();
-				int32 count = polygonShape->m_count;
-				b2Vec2 prev, v;
-
-				for (int32 i = 0; i < count; ++i)
+				switch (f->GetType())
 				{
-					v = b->GetWorldPoint(polygonShape->m_vertices[i]);
-					if (i > 0)
-						DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), RED);
-
-					prev = v;
-				}
-
-				v = b->GetWorldPoint(polygonShape->m_vertices[0]);
-				DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), RED);
-			}
-			break;
-
-			// Draw chains contour -------------------------------------------
-			case b2Shape::e_chain:
-			{
-				b2ChainShape* shape = (b2ChainShape*)f->GetShape();
-				b2Vec2 prev, v;
-
-				for (int32 i = 0; i < shape->m_count; ++i)
+					// Draw circles ------------------------------------------------
+				case b2Shape::e_circle:
 				{
-					v = b->GetWorldPoint(shape->m_vertices[i]);
-					if (i > 0)
-						DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), GREEN);
-					prev = v;
+					b2CircleShape* shape = (b2CircleShape*)f->GetShape();
+					b2Vec2 pos = f->GetBody()->GetPosition();
+
+					DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), (float)METERS_TO_PIXELS(shape->m_radius), Color{ 0, 0, 0, 128 });
+				}
+				break;
+
+				// Draw polygons ------------------------------------------------
+				case b2Shape::e_polygon:
+				{
+					b2PolygonShape* polygonShape = (b2PolygonShape*)f->GetShape();
+					int32 count = polygonShape->m_count;
+					b2Vec2 prev, v;
+
+					for (int32 i = 0; i < count; ++i)
+					{
+						v = b->GetWorldPoint(polygonShape->m_vertices[i]);
+						if (i > 0)
+							DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), RED);
+
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(polygonShape->m_vertices[0]);
+					DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), RED);
+				}
+				break;
+
+				// Draw chains contour -------------------------------------------
+				case b2Shape::e_chain:
+				{
+					b2ChainShape* shape = (b2ChainShape*)f->GetShape();
+					b2Vec2 prev, v;
+
+					for (int32 i = 0; i < shape->m_count; ++i)
+					{
+						v = b->GetWorldPoint(shape->m_vertices[i]);
+						if (i > 0)
+							DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), GREEN);
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(shape->m_vertices[0]);
+					DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), GREEN);
+				}
+				break;
+
+				// Draw a single segment(edge) ----------------------------------
+				case b2Shape::e_edge:
+				{
+					b2EdgeShape* shape = (b2EdgeShape*)f->GetShape();
+					b2Vec2 v1, v2;
+
+					v1 = b->GetWorldPoint(shape->m_vertex0);
+					v1 = b->GetWorldPoint(shape->m_vertex1);
+					DrawLine(METERS_TO_PIXELS(v1.x), METERS_TO_PIXELS(v1.y), METERS_TO_PIXELS(v2.x), METERS_TO_PIXELS(v2.y), BLUE);
+				}
+				break;
+
 				}
 
-				v = b->GetWorldPoint(shape->m_vertices[0]);
-				DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), GREEN);
-			}
-			break;
+				if (mouse_joint == nullptr && mouseSelect == nullptr && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
 
-			// Draw a single segment(edge) ----------------------------------
-			case b2Shape::e_edge:
-			{
-				b2EdgeShape* shape = (b2EdgeShape*)f->GetShape();
-				b2Vec2 v1, v2;
-
-				v1 = b->GetWorldPoint(shape->m_vertex0);
-				v1 = b->GetWorldPoint(shape->m_vertex1);
-				DrawLine(METERS_TO_PIXELS(v1.x), METERS_TO_PIXELS(v1.y), METERS_TO_PIXELS(v2.x), METERS_TO_PIXELS(v2.y), BLUE);
-			}
-			break;
-
-			}
-
-			if (mouse_joint == nullptr && mouseSelect == nullptr && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-
-				if (f->TestPoint(pMousePosition)) {
-					mouseSelect = b;
+					if (f->TestPoint(pMousePosition)) {
+						mouseSelect = b;
+					}
 				}
+
+
 			}
-
-
 		}
+		
 	}
 
 
